76f8f62f46255b31958a3aca653ba7e2e918e789
rahul7011 <champrahul01@gmail.com>
2021-05-30 13:38:38 +0530

    post delete api

--- posts/urls.py
+++ posts/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import PostDetailView, PostListView,PostCreateView,PostUpdateView
+from .views import PostDeleteView, PostDetailView, PostListView,PostCreateView,PostUpdateView
 
 app_name="posts"
 urlpatterns = [
     path('posts/',PostListView.as_view(),name='post-list'),
     path('posts/create/',PostCreateView.as_view(),name='post-create'),
     path('posts/<slug>',PostDetailView.as_view(),name='post-detail'),
-    path('posts/<slug>/update',PostUpdateView.as_view(), name='post-update')
+    path('posts/<slug>/update',PostUpdateView.as_view(), name='post-update'),
+    path('posts/<slug>/delete',PostDeleteView.as_view(), name='post-delete'),
 ]
